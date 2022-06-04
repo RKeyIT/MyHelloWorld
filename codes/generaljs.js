9221218c46a901624854afa3e7841a855099c713
Roman Aleksandrovich <rkeytvp@gmail.com>
2022-06-04 11:09:32 +0200

    Quests1Done
    First tasks doned.

---
+++ codes/generaljs.js
@@ -0,0 +1,14 @@
+//  VARIABLES
+let currentCoins // general moneys in account
+let payday //   Day in date of general money add.
+
+//  ARRAYS
+let addedMoneys = []    //  green values
+let lessedMoneys = []   //  red values
+
+//  OBJECTS
+let coins = {
+    comment : '',   //user comment like 'buying bread'
+    amount : value, //positive number
+    color : green,  //green for positive, red for negative
+}
