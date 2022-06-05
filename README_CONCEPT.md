c4f9deafcfc845ca288fc5f5aaf856501af7f232
Roman Aleksandrovich <rkeytvp@gmail.com>
2022-06-05 11:02:13 +0200

    Some experimental
    Road to accept second part of Quest2

--- README_CONCEPT.md
+++ README_CONCEPT.md
@@ -3,8 +3,8 @@
 
 
 <!-- NOTES -->
-    1. Input with added/lessed moneys must accept only numbers.
-    2. Result from input must be written like Object with "comment", "amount", "color".
+    1. Result from input must be written like Object with "comment", "amount", "color".
+    2. Input with added/lessed moneys must accept only numbers.
 
 <!-- CONCEPTS -->
     Some newest ideas:
--- codes/generaljs.js
+++ codes/generaljs.js
@@ -8,18 +8,47 @@ let lessedMoneys = []   //  red values
 
 //  OBJECTS
 
+let coin = {}
+let coinL = {}
 //example//
 // let coins = {
 //     comment : '',   //user comment like 'buying bread'
 //     amount : value, //positive number
 //     color : green,  //green for positive, red for negative
+//     date : 01.01.01 //date of operation
 // }
 
 //  OPERATIONS
-function calculateThis(){  
-        let changeCoinsOn = document.getElementById("add").value;
-        addedMoneys.push(changeCoinsOn);
+function addCoins() {
+    coin = {
+        comment: document.getElementById('addedFrom').value,
+        amount: document.getElementById("add").value,
+        color: 'green',
+        date: new Date(),
     }
-calculateThis();
-console.log(addedMoneys);
-console.log(lessedMoneys);
+    if(coin.amount === "" || coin.amount === "0") {return}
+    addedMoneys.push(coin)
+}
+
+// FUNCTION IN BOTTOM DIDNT WORK IF WE HAVE "IF"
+// BUT WE CAN USE THIS FUNCTIONЫ ON DIFFERENT PAGES
+function lessCoins() {
+    coinL = {
+        comment: document.getElementById('losedFor').value,
+        amount: document.getElementById("less").value,
+        color: 'red',
+        date: new Date(),
+    }
+    if(coinL.amount === "" || coinL.amount === "0") {return}
+    lessedMoneys.push(coinL)
+}
+
+
+// RELAX TIME...
+function calculateCurrent() {
+    sumAdd
+    for (i = 0 ; i < addedMoneys[-1] ; i++) {
+        sumAdd += addedMoneys[-1]
+    }
+    sumLess =
+}
