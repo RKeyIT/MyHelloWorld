5ed7dc64e74f61930a80bf2fec03139143fd3dd1
Roman Aleksandrovich <rkeytvp@gmail.com>
2022-06-24 07:11:09 +0200

    Quest8AddedCodeAddedStyle
    Added some conceptual color changes
    
    Added firstDayOfWeek funcs
    Added some code.
    
    Next - create code for calendar buildings

--- codes/calendar/calendar.js
+++ codes/calendar/calendar.js
@@ -1,7 +1,6 @@
 let pastTime = new Date(2022, 5, 1);   //  variable for write "first date"
 let formatedDate
 formateDate()
-setFirstDate()
 
 function formateDate() {
     let dd = pastTime.getDate();
@@ -12,11 +11,74 @@ function formateDate() {
         dd = '0'+dd;
     }
 
-    mm = pastTime.toLocaleString('default', {month: 'long'}).toUpperCase();
+    mm = pastTime.toLocaleString('default', {month: 'long'}).charAt().toUpperCase() +
+    pastTime.toLocaleString('default', {month: 'long'}).slice(1); 
 
     formatedDate = `${dd} ${mm} ${yyyy}`;
+    document.getElementById('fullDate').innerHTML = formatedDate;
 }
 
-function setFirstDate() {
-    document.getElementById('fullDate').innerHTML = formatedDate;
+let daysInMonth;
+let firstDayOfCurrentMonth;
+
+function countOfDays() {    //  returns count of days in current month
+    let dateOne = new Date();
+    let dateTwo = dateOne.setDate(dateOne.getDate() + 30); //   optional variable for save changed dateOne variable
+        dateOne = dateOne.getDate();
+
+    if (currentDayOfDate === dateOne){
+        daysInMonth = 30
+    };
+    if (currentDayOfDate === dateOne + 1){
+        daysInMonth = 31
+    };
+    if (currentDayOfDate === dateOne - 2){
+        daysInMonth = 28
+    }; 
+    if (currentDayOfDate === dateOne - 1){
+        daysInMonth = 29
+    };  
+}
+
+function dayOfWeek() {
+    let date1 = new Date().getFullYear();
+    let date2 = new Date().getMonth();
+    firstDayOfCurrentMonth = new Date(date1, date2, 1)
+}
+
+function buildCalendar () {
+    let firstDayPoint = 'd'+firstDayOfCurrentMonth.getDay()
+        /** THIS IS THE FIRST WAY  **/
+        setCalendar()
+        function setCalendar(firstDayPoint) {
+        // build from firstDayPoint
+
+
+        //experimental
+        document.getElementById(`${firstDayPoint}`).innerHTML = 1;
+    }
+
+        /** THIS IS THE SECOND WAY **/
+    /************   BUILD CALCULATOR FROM THIS IF's  ************/
+    if(firstDayPoint = 1) {     //  Monday
+
+    }
+    if(firstDayPoint = 2) {     //  Tuesday
+        
+    }
+    if(firstDayPoint = 3) {     //  Wednesday
+        
+    }
+    if(firstDayPoint = 4) {     //  Thursday
+        
+    }
+    if(firstDayPoint = 5) {     //  Friday
+        
+    }
+    if(firstDayPoint = 6) {     //  Saturday
+        
+    }
+    if(firstDayPoint = 0) {     //  Sunday
+        
+    }
 }
