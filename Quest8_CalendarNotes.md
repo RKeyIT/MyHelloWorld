91982543b83a1abfe036232d57fd5b22f65f1afa
Roman Aleksandrovich <rkeytvp@gmail.com>
2022-06-25 22:57:04 +0200

    Quest8Re:Zero
    New script file been started.
    Added CalendarNotes file for create easy way to Calendar program.

---
+++ Quest8_CalendarNotes.md
@@ -0,0 +1,10 @@
+# MyHelloWorld
+
+Variables, Arrays, Objects...
+
+1. <DONE> Needs 1 changeble variable from page start and rechangeble this after any arrow click or something else. 
+2. <DONE> Needs 3 reusable changeble variables with year, month, day.
+3. <DONE> Needs variable for save Month and Year for showing this into status bar of Calendar.
+4. <DONE> Needs countOfDays in month and day of week of first day in month variables.
+5. <DONE> Needs array for save count of days in month and use this count for build calendar in next.
+6. Function for showing current month and year.
--- codes/calendar/calendar.js
+++ codes/calendar/calendar.js
@@ -1,149 +1,19 @@
-let generalDate = new Date()
-let currentYear = generalDate.getFullYear(); 
-let currentMonth = generalDate.getMonth();
-let currentTime = new Date(currentYear, currentMonth, 1);   //  variable for write "first date" is when i begining this code
+//  Calendar programm
 
+let generalDate = new Date();   // this date will be used for building calendar from start page.
 
-let formatedDate    // variable for show current formate of date
-let currentDayOfDate = new Date().getDate();  // current date of date
-let daysInMonth = 30;                // count of days in current month
-let firstDayOfCurrentMonth;     // monday or friday today?
+let year    = generalDate.getFullYear();    // this variable will be used for setting new dates into calendar
+let month = generalDate.getMonth();         // this variable will be used for setting new dates into calendar
+let day = generalDate.getDate();            // this variable will be used for setting new dates into calendar
 
+let showedDate      // this date will be show Month and Year used now
+let dayOfWeek       // this variable needs to build calendar from correct day of week
+let daysInMonth     // this variable will save count of days into used month now
 
+const dateArray = []    // this array will save count of days and create days into calendar
 
+function showDate() {
+    let showingDate
 
-
-formateDate()
-countOfDays()                   //  count of days in current month
-
-let dateArray = [0];
-
-fillDateArray()
-function fillDateArray() {
-    let numberOfDay = 1;
-    dateArray.length = 1;
-    for(i = 1; i <= daysInMonth; i++){
-        dateArray.push(numberOfDay);
-        numberOfDay++;
-    }
+    generalDate
 }
-
-dayOfWeek()                     //  monday or friday today?
-buildCalendar()                 //  calendar sets
-
-
-
-function formateDate() {                //  func for formatedDate  
-    // let dd = currentTime.getDate();
-    let mm = currentTime.getMonth();
-    let yyyy = currentTime.getFullYear();
-
-    // if(dd<10) {
-    //     dd = '0'+dd;
-    // }
-
-    mm = currentTime.toLocaleString('default', {month: 'long'}).charAt().toUpperCase() +
-    currentTime.toLocaleString('default', {month: 'long'}).slice(1); 
-
-    formatedDate = `${mm} ${yyyy}`;
-    document.getElementById('fullDate').innerHTML = formatedDate;
-};
-
-
-function countOfDays() {    //  returns count of days in current month
-
-    // let d1 = currentTime.getFullYear()
-    // let d2 = currentTime.getMonth()
-    // let d3 = currentTime.getDate() + 30
-    // let dateOne = new Date(d1, d2, 31)
-    let dateOne = new Date();
-    let dateTwo = dateOne.setDate(dateOne.getDate() + 30); //   optional variable for save changed dateOne variable
-        dateOne = dateOne.getDate();
-        dateTwo = currentTime.getDate()
-    if (dateTwo === dateOne){
-        daysInMonth = 30
-    };
-    if (dateTwo === dateOne + 1){
-        daysInMonth = 31
-    };
-    if (dateTwo === dateOne - 2){
-        daysInMonth = 28
-    }; 
-    if (dateTwo === dateOne - 1){
-        daysInMonth = 29
-    };  
-    //     //  GLOBAL VARIABLE WILL BE HERE
-    //     currentDayOfDate = new Date();  // GLOBAL VARIABLE WITH CYCLIC CALLS
-    //     currentDayOfDate = currentDayOfDate.getDate();
-    //     //  GLOBAL VARIABLE WAS HERE
-
-    // if (currentDayOfDate === dateOne){
-    //     daysInMonth = 30
-    // };
-    // if (currentDayOfDate === dateOne + 1){
-    //     daysInMonth = 31
-    // };
-    // if (currentDayOfDate === dateOne - 2){
-    //     daysInMonth = 28
-    // }; 
-    // if (currentDayOfDate === dateOne - 1){
-    //     daysInMonth = 29
-    // };  
-}
-function dayOfWeek() {      // what is the day of week of current or new Month? Friday?
-    let date1 = currentTime.getFullYear();
-    let date2 = currentTime.getMonth();
-    firstDayOfCurrentMonth = new Date(date1, date2, 1)
-}
-
-function buildCalendar() {
-    eq = 1;
-    for(i = 1; i < dateArray.length; i++){
-        let numOfDay = 'd'+(firstDayOfCurrentMonth.getDay() - 1 + i)
-        document.getElementById(numOfDay).innerHTML = i;
-        eq++
-    };
-
-    let date0 = new Date();
-    let date1 = new Date(date0.getFullYear(), date0.getMonth(), 0);
-    let prevMonthShadow = date1.getDate();    
-    for(i = firstDayOfCurrentMonth.getDay() - 1; i > 1; i--) {
-        let numOfDay = 'd'+i;
-        document.getElementById(numOfDay).innerHTML = prevMonthShadow--;
-        eq++
-    }
-    let nextMonthShadow = 1
-    for(i = eq; i <= 42; i++) {
-        document.getElementById('d'+i).innerHTML = nextMonthShadow++;
-    }
-    //build from firstDayPoint
-}
-
-
-function generatePrevMonth() {
-    currentMonth--;
-    currentTime = new Date(currentYear, currentMonth, 1)
-    fullRecall()
-}
-function generateNextMonth() {
-    currentMonth++;
-    currentTime = new Date(currentYear, currentMonth, 1)
-    fullRecall()
-}
-
-function fullRecall() {
-    formateDate();      // 1.
-    countOfDays();      // 2.
-    dayOfWeek();        // 3.
-    fillDateArray();    // 4.
-    buildCalendar();    // 5.
-}
-
-/*
-FOR THIS CODE NEEDS:
-    1) generalDate variable
-    2) changeble variable with date for any fucns
-    3) Each func can update changeble variable date
-
-
-*/
