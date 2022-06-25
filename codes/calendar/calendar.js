5d3cf38a91efb4c32be1fe817674f87326747cfb
Roman Aleksandrovich <rkeytvp@gmail.com>
2022-06-25 11:45:34 +0200

    Quest8AddedAutoFill
    AutoFill for calendar been added but is not a good. Lets work soon

--- codes/calendar/calendar.js
+++ codes/calendar/calendar.js
@@ -1,8 +1,15 @@
-let pastTime = new Date(2022, 5, 1);   //  variable for write "first date" is when i begining this code
+let generalDate = new Date()
+let currentYear = generalDate.getFullYear(); 
+let currentMonth = generalDate.getMonth();
+let currentTime = new Date(currentYear, currentMonth, 1);   //  variable for write "first date" is when i begining this code
+
+
 let formatedDate    // variable for show current formate of date
 let currentDayOfDate = new Date().getDate();  // current date of date
 let daysInMonth;                // count of days in current month
 let firstDayOfCurrentMonth;     // monday or friday today?
+
+
 
 
 
@@ -14,6 +21,7 @@ let dateArray = [0];
 fillDateArray()
 function fillDateArray() {
     let numberOfDay = 1;
+    dateArray.length = 1;
     for(i = 1; i <= daysInMonth; i++){
         dateArray.push(numberOfDay);
         numberOfDay++;
@@ -26,18 +34,18 @@ buildCalendar()                 //  calendar sets
 
 
 function formateDate() {                //  func for formatedDate  
-    let dd = pastTime.getDate();
-    let mm = pastTime.getMonth();
-    let yyyy = pastTime.getFullYear();
+    // let dd = currentTime.getDate();
+    let mm = currentTime.getMonth();
+    let yyyy = currentTime.getFullYear();
 
-    if(dd<10) {
-        dd = '0'+dd;
-    }
+    // if(dd<10) {
+    //     dd = '0'+dd;
+    // }
 
-    mm = pastTime.toLocaleString('default', {month: 'long'}).charAt().toUpperCase() +
-    pastTime.toLocaleString('default', {month: 'long'}).slice(1); 
+    mm = currentTime.toLocaleString('default', {month: 'long'}).charAt().toUpperCase() +
+    currentTime.toLocaleString('default', {month: 'long'}).slice(1); 
 
-    formatedDate = `${dd} ${mm} ${yyyy}`;
+    formatedDate = `${mm} ${yyyy}`;
     document.getElementById('fullDate').innerHTML = formatedDate;
 };
 
@@ -47,26 +55,26 @@ function countOfDays() {    //  returns count of days in current month
     let dateTwo = dateOne.setDate(dateOne.getDate() + 30); //   optional variable for save changed dateOne variable
         dateOne = dateOne.getDate();
 
-    if (currentDayOfDate === dateOne){
+    if (currentTime === dateOne){
         daysInMonth = 30
     };
-    if (currentDayOfDate === dateOne + 1){
+    if (currentTime === dateOne + 1){
         daysInMonth = 31
     };
-    if (currentDayOfDate === dateOne - 2){
+    if (currentTime === dateOne - 2){
         daysInMonth = 28
     }; 
-    if (currentDayOfDate === dateOne - 1){
+    if (currentTime === dateOne - 1){
         daysInMonth = 29
     };  
 }
 function dayOfWeek() {      // what is the day of week of current or new Month? Friday?
-    let date1 = new Date().getFullYear();
-    let date2 = new Date().getMonth();
+    let date1 = currentTime.getFullYear();
+    let date2 = currentTime.getMonth();
     firstDayOfCurrentMonth = new Date(date1, date2, 1)
 }
 
-function buildCalendar () {
+function buildCalendar() {
     eq = 1;
     for(i = 1; i < dateArray.length; i++){
         let numOfDay = 'd'+(firstDayOfCurrentMonth.getDay() - 1 + i)
@@ -76,15 +84,44 @@ function buildCalendar () {
 
     let date0 = new Date();
     let date1 = new Date(date0.getFullYear(), date0.getMonth(), 0);
-    let x = date1.getDate();    
+    let prevMonthShadow = date1.getDate();    
     for(i = firstDayOfCurrentMonth.getDay(); i > 1; i--) {
         let numOfDay = 'd'+(i - 1);
-        document.getElementById(numOfDay).innerHTML = x--;
+        document.getElementById(numOfDay).innerHTML = prevMonthShadow--;
         eq++
     }
-    let f = 1
+    let nextMonthShadow = 1
     for(i = eq; i <= 35; i++) {
-        document.getElementById('d'+i).innerHTML = f++;
+        document.getElementById('d'+i).innerHTML = nextMonthShadow++;
     }
     //build from firstDayPoint
 }
+
+
+function generatePrevMonth() {
+    currentMonth--;
+    currentTime = new Date(currentYear, currentMonth, 1)
+    formateDate();
+    countOfDays();
+    dayOfWeek();
+    fillDateArray();
+    buildCalendar()
+}
+function generateNextMonth() {
+    currentMonth++;
+    currentTime = new Date(currentYear, currentMonth, 1)
+    formateDate();
+    countOfDays();
+    dayOfWeek();
+    fillDateArray();
+    buildCalendar()
+}
+
+/*
+FOR THIS CODE NEEDS:
+    1) generalDate variable
+    2) changeble variable with date for any fucns
+    3) Each func can update changeble variable date
+
+
+*/
