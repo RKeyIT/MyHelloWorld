89eab2a24f47260d0dcc98f4d377e8338f602ead
Roman Aleksandrovich <rkeytvp@gmail.com>
2022-06-26 14:55:13 +0200

    Quest8CalendarDONED
    Calendar been created.
    
    Welcome to brainstorm again :)

--- Quest8_CalendarNotes.md
+++ Quest8_CalendarNotes.md
@@ -7,4 +7,11 @@ Variables, Arrays, Objects...
 3. <DONE> Needs variable for save Month and Year for showing this into status bar of Calendar.
 4. <DONE> Needs countOfDays in month and day of week of first day in month variables.
 5. <DONE> Needs array for save count of days in month and use this count for build calendar in next.
-6. Function for showing current month and year.
+6. <DONE> Function for showing current month and year.
+7. <DONE> Calculate count of days into active month. (function countOfDays)
+8. <DONE> Strong updated <countOfDays> function been added.
+9. <DONE> dayOfWeek variable and function.
+10. <DONE> Build Calendar.
+11. <DONE> Build shadow days.
+
+    <Full rework for 10-11 tasks. Calendar taked big update...>
--- codes/calendar/calendar.js
+++ codes/calendar/calendar.js
@@ -6,14 +6,84 @@ let year    = generalDate.getFullYear();    // this variable will be used for se
 let month = generalDate.getMonth();         // this variable will be used for setting new dates into calendar
 let day = generalDate.getDate();            // this variable will be used for setting new dates into calendar
 
+let currentTime = new Date(year, month, 1)  // sets short date for next works with calendar
+
 let showedDate      // this date will be show Month and Year used now
 let dayOfWeek       // this variable needs to build calendar from correct day of week
 let daysInMonth     // this variable will save count of days into used month now
 
 const dateArray = []    // this array will save count of days and create days into calendar
 
-function showDate() {
-    let showingDate
+/******************CALENDAR AUTOSTART*********************/
+autoStart()
+function autoStart(){
+    showDate();
+    countOfDays();
+    fDayOfWeek();
+    buildCalendar();
+};
+/******************CALENDAR AUTOSTART*********************/
 
-    generalDate
+function showDate() {
+    let mm = currentTime.toLocaleString('default', {month: 'long'}).charAt().toUpperCase() + 
+        currentTime.toLocaleString('default', {month: 'long'}).slice(1);
+    let yyyy = currentTime.getFullYear();
+    let showingDate = mm + ' ' + yyyy;
+    document.getElementById('fullDate').innerHTML = showingDate;
+}
+
+function countOfDays() {    //  returns count of days in current month
+    daysInMonth = new Date(year, month + 1, 0).getDate()
+}
+
+function fDayOfWeek() {
+    dayOfWeek = currentTime.getDay()    // 0 1 2 3 4 5 6
+}
+
+function buildCalendar() {
+    let idOfDay                     // this is the day of week. From this day will be create any month of calendar
+
+/*****************PREV MONTH SHADOW**************************/
+    daysInMonth = new Date(year, month, 0).getDate()
+    dayOfWeek   = new Date(year, month, 0).getDay()
+    for(i = daysInMonth; dayOfWeek > 0; i--) {
+        idOfDay = 'd' + dayOfWeek--;
+        document.getElementById(idOfDay).innerHTML = i;
+    }
+/*****************PREV MONTH SHADOW**************************/
+
+/*****************CURRENT MONTH NORMAL**************************/
+    daysInMonth = new Date(year, month + 1, 0).getDate()
+    dayOfWeek   = new Date(year, month, 1).getDay()
+    for(i = 1; i <= daysInMonth; i++) {
+        if(dayOfWeek === 0) {
+            dayOfWeek = 7
+            idOfDay = 'd' + dayOfWeek;
+        } else {
+            idOfDay = 'd' + dayOfWeek;
+        };
+        dayOfWeek++
+        document.getElementById(idOfDay).innerHTML = i;
+        
+    }
+/*****************CURRENT MONTH NORMAL**************************/
+
+/*****************NEXT MONTH SHADOW**************************/
+    let nextMonthShadow = dayOfWeek;
+    let shadowNumber = 1
+    for(i = nextMonthShadow; i < 43; i++) {
+        idOfDay = 'd' + nextMonthShadow++;
+        document.getElementById(idOfDay).innerHTML = shadowNumber++;
+    }
+/*****************NEXT MONTH SHADOW**************************/
+}
+
+function nextMonth(){
+    currentTime = new Date(year, ++month, 1)
+    autoStart()
+}
+
+function prevMonth(){
+    currentTime = new Date(year, --month, 1)
+    autoStart()
 }
