beaf686617851f050c3c0b254709f48f42b81bb9
Roman Aleksandrovich <rkeytvp@gmail.com>
2022-07-04 07:02:56 +0200

    REbuildForFullAPP_01
    This needs for better understandings and rewrite vars. consts and funcs. Delete needless and create new (better).

--- README_GENERAL.md
+++ README_GENERAL.md
@@ -16,31 +16,21 @@ That needs for peoples whos can't save a lot
 of theys moneys by themselfs for future.
 
 [GENERAL]
-[TODOS]
+[DESCRIPTION_LIST]
 
-Arrays, Objects, Variables:
+1. For easilly app control files was separated:
+    1) 01vars.js: This file will be save any of global variable with description. [NotALocalVars]
+    2) 02classes.js: Classes and unique menthods (& functions). 
+    3) 03noautocallfuncs.js: This functions will be read in first and will used later into app
+    4) 04autocallfuncs.js: This is constructor for app from page start.
+   
+   This files has numbers for create better queue of looadings.
 
-**Arrays**:
-
-Arrays use word **Moneys** for example 'addedMoneys'
-
-FOR HISTORY:
-
-lessedMoneys[] **RedColor**
-addedMoneys[] **GreenColor**
-
-Test
-savedMoneys[] **BlueColor**
-
-**Objects**
-
-Elements for history arrays
-For differencial options:
-
-coins = {
-    comment: Buying bred / Present by president
-    amount: 43  (it will be only natural positive)
-    color: green/red    (+ or - symbols)
-}
-
-**Variables**
+2. FULL descriptions list for used names (vars, funcs and other)
+    1) Variables:
+       1) generalDate - [UNCHANGEBLE] - current date. From this variable begins all date calculatings.
+       2) year, month, day - [changeble] - operative dates for dynamic calculatings with changes from a lot of funcs.
+       3) currentTime - [?changeble] - like a general date. But needs for dynamic calendar buildigs. [IsThisReallyNeeds?]
+       4) dayOfWeek - [changeble] - save day (like a 'Monday') and return this for next calculator building process.
+       5) daysInMonth - [changeble] - save count of days into operative month.
+       6) 
---
+++ codes/js/02classes.js
@@ -0,0 +1,72 @@
+class Coin {        // Class and after this functions for this class
+    constructor(){
+        this.date = new Date();
+    }
+    take(){
+        this.amount = Number(document.getElementById('add').value),
+        this.comment = document.getElementById('addedFrom').value,
+        this.color = 'green';
+        MONEYS.push(coin)
+    }
+    spend(){
+        this.amount = Number(document.getElementById("less").value),
+        this.comment = document.getElementById('losedFor').value,
+        this.color = 'red';
+        MONEYS.push(coin)
+    }
+    save(){
+        this.amount = Number(document.getElementById('save').value),
+        this.comment = document.getElementById('saveFor').value,
+        this.color = 'blue';
+        MONEYS.push(coin)
+    }
+}
+//Operative functions with this class
+    function takeCoin() {
+        if (
+            document.getElementById('add').value === '' || 
+            Number(document.getElementById('add').value) !== 
+            Number(document.getElementById('add').value) || 
+            document.getElementById('add').value <= 0
+            ) {return}
+        else {
+        coin = new Coin()
+        coin.take();
+        calculateCurrentMoneys();
+        spendPerDay();
+        document.getElementById('add').value = '';
+        document.getElementById('addedFrom').value = '';
+        }
+    }
+    function spendCoin() {
+        if (
+            document.getElementById('less').value === '' || 
+            Number(document.getElementById('less').value) !== 
+            Number(document.getElementById('less').value) || 
+            document.getElementById('less').value <= 0
+            ) {return}
+        else {
+        coin = new Coin ()
+        coin.spend();
+        calculateCurrentMoneys();
+        spendPerDay();
+        document.getElementById('less').value = '';
+        document.getElementById('losedFor').value = '';
+        }
+    }
+    function saveCoin() {
+        if (
+            document.getElementById('save').value === '' || 
+            Number(document.getElementById('save').value) !== 
+            Number(document.getElementById('save').value) || 
+            document.getElementById('save').value <= 0
+            ) {return}
+        else {
+        coin = new Coin ()
+        coin.save();
+        calculateCurrentMoneys();
+        spendPerDay();
+        document.getElementById('save').value = '';
+        document.getElementById('saveFor').value = '';
+        }
+    }
